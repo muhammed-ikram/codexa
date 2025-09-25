7e9e2aba63ed9941319da8eeb402cc223e30f7ad
mukesh chowdary <msai81398@gmail.com>
2025-09-26 04:49:59 +0530

    frontend pro18

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cors({
     return callback(new Error('Not allowed by CORS'));
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
 
